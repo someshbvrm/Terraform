5727be0283c3f5b6f2818c592c8d53f86977534f
David <somesh910@gmail.com>
2025-08-02 11:33:52 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+/*
+resource "aws_instance" "app_server" {
+  ami = data.aws_ami.ubuntu.id
+  # instance_type = "t2.micro"
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
+  tags = {
+    #  Name = "learn-terraform"
+    Name = var.instance_name
+  }
+}
+*/
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames = true
+}
