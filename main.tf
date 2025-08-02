874c692f8e0720fa8790cb8da7b2b032f1c5a038
David <somesh910@gmail.com>
2025-08-02 14:44:04 +0530

    my second commit

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-/*
+
 resource "aws_instance" "app_server" {
   ami = data.aws_ami.ubuntu.id
   # instance_type = "t2.micro"
@@ -25,7 +25,6 @@ resource "aws_instance" "app_server" {
     Name = var.instance_name
   }
 }
-*/
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.19.0"
